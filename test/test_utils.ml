3220941410dba7fee619f3961eb9d01947db683d
Donatas Petrauskas <donatas.petr@gmail.com>
2017-10-09 23:34:09 +0300

    Read actual file list and serve it.

---
+++ test/test_utils.ml
@@ -0,0 +1,66 @@
+open Core
+open Cohttp_lwt_unix
+
+type file_tree =
+  | Directory of string * file_tree list
+  | File of string * string
+
+let with_server port spec test =
+  let server = Server.make ~callback:(fun _ req body -> spec req body) () in
+  let uri = Uri.of_string ("http://0.0.0.0:" ^ (string_of_int port)) in
+  let server_failed, server_failed_wake = Lwt.task () in
+  let server =
+    Lwt.catch
+      (fun () -> Server.create ~mode:(`TCP (`Port port)) server)
+      (function
+        | Lwt.Canceled -> Lwt.return_unit
+        | x -> Lwt.wakeup_exn server_failed_wake x; Lwt.fail x)
+  in
+  Lwt.(pick [ test uri; server_failed ] >|= fun res -> cancel server; res)
+
+let http_get uri =
+  let open Lwt in
+  Lwt_main.run begin
+    Client.get uri
+    >>= fun (resp, body) -> Cohttp_lwt.Body.to_string body
+  end
+
+let char_range a z =
+  let l = Char.to_int a and r = Char.to_int z in
+  if l <= r then
+    let result = String.create (r - l + 1) in
+    List.range ~stop:`inclusive l r
+    |> List.iteri ~f:(fun i n -> result.[i] <- Char.of_int_exn n);
+    result
+  else
+    ""
+
+let random_string () =
+  let source =
+    String.concat_array
+      [| char_range '0' '9'; char_range 'a' 'z'; char_range 'A' 'Z'; "_-"|] in
+  let source_len = String.length source in
+  let output = String.make 20 '_' in
+  for i = 0 to 19 do
+    output.[i] <- source.[Random.int source_len]
+  done;
+  output
+
+let make_file_tree spec =
+  let ( / ) = FilePath.concat in
+  let open FileUtil in
+  let rec tmp_file_tree_1 workdir spec =
+    match spec with
+    | Directory (name, files) ->
+      let parent = workdir / name in
+      mkdir parent;
+      List.iter files (tmp_file_tree_1 parent)
+    | File (name, _) ->
+      touch (workdir / name)
+  in
+  let tmpdir = FilePath.make_filename [Sys.getenv_exn "TMPDIR"] in
+  let workdir = tmpdir / ("butler-" ^ random_string ()) in
+  if not (test Exists workdir) then
+    mkdir workdir;
+  tmp_file_tree_1 workdir spec;
+  workdir
--- test/tests.ml
+++ test/tests.ml
@@ -1,40 +1,37 @@
 open Core
 open Cohttp_lwt_unix
 
-let http_get uri =
-  let open Lwt in
-  Lwt_main.run begin
-    Client.get uri
-    >>= fun (resp, body) -> Cohttp_lwt.Body.to_string body
-  end
-
-let with_server port spec test =
-  let server = Server.make ~callback:(fun _ req body -> spec req body) () in
-  let uri = Uri.of_string ("http://0.0.0.0:" ^ (string_of_int port)) in
-  let server_failed, server_failed_wake = Lwt.task () in
-  let server = Lwt.catch
-                 (fun () -> Server.create ~mode:(`TCP (`Port port)) server)
-                 (function
-                   | Lwt.Canceled -> Lwt.return_unit
-                   | x -> Lwt.wakeup_exn server_failed_wake x; Lwt.fail x)
-  in
-  Lwt.(pick [ test uri; server_failed ] >|= fun res -> cancel server; res)
-
 let serve_index () =
+  let spec =
+    let open Test_utils in
+    let files =
+      [ File ("index.html", "")
+      ; File ("style.css", "")
+      ; File ("main.js", "")
+      ]
+    in
+    Directory ("public", files)
+  in
+  let workdir = Test_utils.make_file_tree spec in
   Lwt_main.run (
-    with_server 8080
+    Test_utils.with_server 8080
       (fun _ _ ->
-         let body = Sexp.to_string [%sexp ["index.html"; "style.css"; "main.js"]] in
+         let files =
+           let prefix = FilePath.concat workdir "public" in
+           let prefix_len = String.length prefix in
+           FileUtil.ls prefix
+           |> List.map ~f:(fun name -> String.drop_prefix name prefix_len)
+         in
+         let body = Sexp.to_string (sexp_of_list sexp_of_string files) in
          Server.respond_string ~status:`OK ~body ())
       (fun uri ->
          Lwt.return (
            Alcotest.(check (list string)) "correct list of files"
-             ["index.html"; "style.css"; "main.js"]
-             (http_get uri
+             ["/index.html"; "/main.js"; "/style.css"]
+             (Test_utils.http_get uri
               |> Sexp.of_string
               |> list_of_sexp string_of_sexp)
          )))
-
 
 let test_serving =
   [ "index" , `Slow, serve_index
