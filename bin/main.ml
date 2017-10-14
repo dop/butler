6fd4254e2fd1f1a7e3cf93fa58fff0ec55c447cc
Donatas Petrauskas <donatas.petr@gmail.com>
2017-10-14 14:41:37 +0300

    Use cmdliner.

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,35 @@
+open Cmdliner
+
+let main serve config_file directory =
+  Butler.run ~serve ~config_file directory
+
+let main_t =
+  let serve = Arg.(
+      value & flag & info ["s"; "serve"]
+        ~doc:"Start file server."
+    )
+  in
+  let config_file = Arg.(
+      value & opt string "butler" & info ["c"; "config"]
+        ~docv:"FILE"
+        ~doc:"Configuration file."
+    )
+  in
+  let directory = Arg.(
+      value & pos 0 string "." & info []
+        ~docv:"DIR"
+        ~doc:"Directory to run in. Defaults to current directory."
+    )
+  in
+  Term.(const main $ serve $ config_file $ directory)
+
+let info =
+  let doc = "run commands on file changes." in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <donatas.petr at gmail.com>." ]
+  in
+  Term.info "butler" ~version:"%‌%VERSION%%" ~doc ~exits:Term.default_exits ~man
+
 let () =
-  Butler.run ~config_file:"butler" "."
+  Term.exit @@ Term.eval (main_t, info)
