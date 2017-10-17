f2513fa130a7769e9380a720afbfa1391e279b12
Donatas Petrauskas <donatas.petr@gmail.com>
2017-10-17 09:28:24 +0300

    Drop interface for now.

--- lib/butler.mli
+++
@@ -1,7 +0,0 @@
-open Cohttp_lwt
-
-val make_file_server : string -> Request.t -> 'a -> (Response.t * Body.t) Lwt.t
-
-val run : ?serve:bool -> config_file:string -> string -> unit
-
-val watch_files : string -> ('a Lwt.u -> string -> unit Lwt.t) -> unit
